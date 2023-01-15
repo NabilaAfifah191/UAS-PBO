2917b4f99b3e2c2b084801bcc4ae43ad58f97bfd
nabilaafifah191 <divarah.loviane@gmail.com>
2023-01-15 14:41:45 +0800

    UAS

---
+++ FDA_E020320021/app/src/main/java/id/ac/poliban/mi/nabila/va/fda_e020320021/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.nabila.va.fda_e020320021.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project82_E020320021/app/src/main/java/id/ac/poliban/mi/nabila/va/project82_e020320021/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.nabila.va.project82_e020320021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
